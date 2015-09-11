17adb4f488f78e1796f670df95da61a4b2b5feda
petnwil <peter.n.wilhelmsen@gmail.com>
2015-09-11 16:06:12 +1000

    Add semicolon in functionButtons

--- client/controllers/functionButtons.js
+++ client/controllers/functionButtons.js
@@ -2,13 +2,13 @@
     Session.set("btnDel", "btn-danger");
     Session.set("btnHistory", "btn-info");
     Session.set("btnOn", "btn-success");
-  
+
     Template.functionButtons.helpers({
         "btnDel" : function(){return Session.get("btnDel");},
         "btnHistory" : function(){return Session.get("btnHistory");},
         "btnOn" : function(){return Session.get("btnOn");}
     });
-    
+
     Template.functionButtons.events({
         "click #btnDel": function(){
             console.log("Delte");
@@ -17,12 +17,7 @@
             console.log("HISTORY");
         },
         "click #btnOn": function(){
-           console.log("BTNON"); 
+           console.log("BTNON");
         }
-        
-    })
-    
 
-
-
-
+    });
