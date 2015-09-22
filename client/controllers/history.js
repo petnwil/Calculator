8ab41986afe1fb380f9877a6e3035f0c2bed612d
haakonW <haakonwinther@gmail.com>
2015-09-22 13:01:32 +1000

    limit 10

--- client/controllers/history.js
+++ client/controllers/history.js
@@ -1,5 +1,8 @@
 Template.history.helpers({
   calculations: function(){
-    return historie.find({}, {sort: {_id: 1}});
+    return historie.find({}, {sort: {_id: -1},limit:10});
+     //return historie.find().limit(10);
+    // return historie.find().limit(10).sort({_id: 1});
+    // db.bios.find().limit( 5 ).sort( { name: 1 } )
   }
 });
